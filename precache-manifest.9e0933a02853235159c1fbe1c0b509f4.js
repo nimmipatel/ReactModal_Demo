197b316fa8dfdbde1c00df119d6690bdd5d927da
nimmirsinsights <nimmi.patel@riversideinsights.com>
2021-09-28 09:27:37 -0500

    Updates

---
+++ precache-manifest.9e0933a02853235159c1fbe1c0b509f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d49700c316d7b851dc4e094c3540edf2",
+    "url": "/ReactModal_Demo/index.html"
+  },
+  {
+    "revision": "d3a4e24ff46d4921304e",
+    "url": "/ReactModal_Demo/static/css/main.e14a2ede.chunk.css"
+  },
+  {
+    "revision": "c872d0ddfdfed7f58c7b",
+    "url": "/ReactModal_Demo/static/js/2.1152e19f.chunk.js"
+  },
+  {
+    "revision": "425f71bb909b45cb21829b79476852ea",
+    "url": "/ReactModal_Demo/static/js/2.1152e19f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3a4e24ff46d4921304e",
+    "url": "/ReactModal_Demo/static/js/main.6982234e.chunk.js"
+  },
+  {
+    "revision": "be6782ce6ab2d1e41bed",
+    "url": "/ReactModal_Demo/static/js/runtime-main.b74944c7.js"
+  },
+  {
+    "revision": "d4d91305dde3681a524bde7c414d6164",
+    "url": "/ReactModal_Demo/static/media/rInsights.d4d91305.svg"
+  }
+]);
